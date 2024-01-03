455dee369830aeaa7dfa4672653a1a40c8bcc726
Femi Adeniran <66181395+phenomenon0@users.noreply.github.com>
2024-01-03 09:27:23 -0600

    Database added to API code
    Sample Database schema is added.
    After practice run -- there will be time to plan the actual schema of the database

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+app.app_context().push()
+
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    country = db.Column(db.String(80), nullable=False)
+    position = db.Column(db.String(15), nullable=False)
+
+    def __repr__(self):
+        return f'{self.name} - {self.country} - {self.position}'
 
 @app.route('/')
 def index():
     return 'Hello, World!'
 
-@app.route('/soccerbase')
+@app.route('/players')
 def soccerbase():
     return {'Player': 'Country'}
